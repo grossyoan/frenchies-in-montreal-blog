c978946eb54e88b3e232c5c3daf28c9ef5171064
majejam <t.f.m.lacroix@gmail.com>
2021-05-16 22:55:54 +0200

    :sparkles: Added youtube embed

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Figure } from "./Figure";
+import Figure from "./Figure";
+import Code from "./Code";
+import getYouTubeId from "get-youtube-id";
+import YouTube from "react-youtube";
 
 const serializers = {
   types: {
-    /* eslint-disable-next-line react/display-name */
     authorReference: ({ node }) => <span>{node.author.name}</span>,
     mainImage: Figure,
+    code: Code,
+    youtube: ({ node }) => {
+      const { url } = node;
+      const id = getYouTubeId(url);
+      return <YouTube videoId={id} />;
+    },
   },
 };
 
